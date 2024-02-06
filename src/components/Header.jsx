e87c98dfce6d078b3d56e8e9ea873ae22179cbcb
Meikaku77 <147025537+Meikaku77@users.noreply.github.com>
2024-02-06 18:10:33 +0100

    arreglos 2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = () => {
   return (
     <>
-    <header className="container  sm:ml-11 justify-center mt-3 mb-3 flex-wrap flex-col gap-3 inline-block">
+    <header className="container justify-center mt-3 mb-3 flex-wrap flex-col gap-3 inline-block">
       <h1 className="text-8xl text-white text-center">SINGULAR </h1>
       <p className="text-6xl text-white text-center ml-8">Jazz en viu</p> 
       <p className="text-white text-3xl text-center ml-8">+34 689593263</p>
