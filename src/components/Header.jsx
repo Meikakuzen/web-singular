08cdb58ec7cd14a4e367a518b2df2ee6c8acd73c
Meikaku77 <147025537+Meikaku77@users.noreply.github.com>
2024-02-06 19:21:41 +0100

    cambio numero tlf

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,8 +5,8 @@ const Header = () => {
     <>
     <header className="container justify-center mt-3 mb-3 flex-wrap flex-col gap-3 inline-block">
       <h1 className="text-8xl text-white text-center">SINGULAR </h1>
-      <p className="text-6xl text-white text-center ml-8">Jazz en viu</p> 
-      <p className="text-white text-3xl text-center ml-8">+34 689593263</p>
+      <p className="text-6xl text-white text-center">Jazz en viu</p> 
+      <p className="text-white text-3xl text-center">+34 644338277</p>
     </header>
     <hr className="border-white border-solid border-2 ml-10 mr-10" />
     </>
