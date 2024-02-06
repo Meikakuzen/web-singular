40c83d610978246dc45e050f400f85d77a2fcd9c
Meikaku77 <147025537+Meikaku77@users.noreply.github.com>
2024-02-06 17:43:06 +0100

    base vite,config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/web-singular/'
+  base: 'https://Meikakuzen.github.io/web-singular/'
 })
